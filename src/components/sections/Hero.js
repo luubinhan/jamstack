094bf54d5a822d29c5ca0c53a3d201422a3c512a
Lưu Bình An <luubinhan1989@gmail.com>
2021-01-03 11:19:10 +0700

    update content

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -70,7 +70,7 @@ const Hero = ({
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                  Tìm hiểu tại sao
+                  Tìm hiểu
                   </Button>
                 </ButtonGroup>
               </div>
